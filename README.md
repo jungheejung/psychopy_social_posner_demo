dd38aca5c1c038176393fd37416303386d24521e
jungheejung <heejung.jung@colorado.edu>
2020-11-09 21:47:39 -0500

    posner cueing paradigm demo

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # SI.01
-special interests
+
+## description
+This experiment is a modified posner cueing paradigm with social cues facilitating the target search.
+
+### conditions
+The demo includes two cue types:
+* one arrow (as a control),
+* one cartoon face as a social cue.
+
+### condition parameters
+experimental parameters are saved in the `design` folder. The script pulls parameters from the `design` .csv files. The csv file is imported as a pandas dataframe and the values are utilized for each trial.
+
+In conjunction, the participant response is also saved as a pandas dataframe.
+
+### output location
+The entire dataframe will be saved in `data` > `sub-ID` > .csv
+
+```
+├── README.md
+├── data
+│   └── sub-01
+│       └── practice.csv
+├── design
+│   └── task-pracetice.csv
+├── scripts
+│   └── psychopy
+│       └── spatialcueing_practice.py
+└── stimuli
+    ├── arrow
+    │   ├── left.png
+    │   ├── neutral.png
+    │   └── right.png
+    └── practice
+        ├── left.png
+        ├── neutral.png
+        └── right.png
+```
--- scripts/psychopy/spatialcueing_practice.py
+++ scripts/psychopy/spatialcueing_practice.py
@@ -1,5 +1,16 @@
-from psychopy import core, visual, gui,event
-#from psychopy.hardware import keyboard
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This is a practice test for the social posner cueing paradigm
+"""
+
+__author__ = "Heejung Jung"
+__version__ = "1.0.1"
+__email__ = "heejung.jung@colorado.edu"
+__status__ = "Production"
+
+from psychopy import core, gui, visual, event
 from random import choice
 import time
 import os
@@ -8,8 +19,7 @@ import pandas as pd
 # parameters ___________________________________________________________________
 
 # 1) directory
-main_dir = "/Users/mristimulus/Desktop/SpecInt/SI.01_git"
-#main_dir = "/Users/h/Dropbox/Projects/SI.01_git"
+main_dir = "/Users/h/Dropbox/projects_dropbox/SI.01_git_forcanlab"
 stimuli_dir = main_dir + os.sep + "stimuli"
 
 # 2) color
@@ -21,32 +31,36 @@ GREEN = [0, 255, 0]
 RED = [255, 0, 0] #[1,-1,-1]
 cue_color = YELLOW
 background_color = GREY
+
 # 3) window
 disp = visual.Window(pos=(0, 0), color=background_color, colorSpace="rgb255", units="norm", fullscr=True)
-# , fullscr=True
+
 # 4) fixation
-fixation_cross = visual.TextStim(win = disp, text = '+', color = [-1,-1,-1], height = 0.2)
-# 6) target
+fixation_cross = visual.TextStim(win = disp, text = '+', color = [-1,-1,-1], height = 0.3)
+
+# 5) target
 target = visual.Circle(win = disp, units = 'pix', radius = 15, edges=128, lineColor=[-1,-1,-1], lineColorSpace='rgb', fillColor=[-1,-1,-1], fillColorSpace='rgb')
 target_positions = {'left': (int(disp.size[0] * -0.2), 0), 'right':( int(disp.size[0] * 0.2), 0)}
-# 7) keyboard mapp
+
+# 6) keyboard map
 key_map = {'1': 'left', '2': 'right'}
 
 
-# start experiment
+# start experiment ___________________________________________________________________
 # 1. prompt ___________________________________________________________________
-#session_info = {'Observer': 'Type observer ID', 'Participant': 'Type participant ID'}
+# if this crashes, try installing PyQt5: pip install PyQt5
+# but make sure to install specific version, otherwise clashes with other libraries
+#https://github.com/CorentinJ/Real-Time-Voice-Cloning/issues/109
+#session_info = {'subject_id': 'Type Participant ID', 'RA_Initials': 'Type RA initials'}
 #date_Str = time.strftime("%b_%d_%H%M", time.localtime())  # add the current time
-#file_prefix = date_Str + ' pcpnt_' + session_info['Participant'] + '_obsvr_'+session_info['Observer']
-#dlg_box = gui.DlgFromDict(session_info, title="Spatial Cueing Paradigm", fixed=["date"])
+#file_prefix = date_Str + ' sub-' + session_info['subject_id'] + '_obsvr_'+session_info['RA_Initials']
+#infoDlg = psychopy.gui.DlgFromDict(dictionary = session_info, title = "Spatial Cueing Paradigm", fixed = ['subject_id'])
 
 # ______________________________________________________________________________
-
-# FOR LOOP START
-condition_list = ['arrow', 'parent_CJ', 'parent_JW', 'interest_CJ', 'interest_JW']
+condition_list = ['practice', 'arrow'] # two conditions = equivalent of block runs
 for ind, cond in enumerate(condition_list):
     # condition specific parameters ____________________________________________________
-    # 1) cue55
+    # 1) cue
     img_left = os.sep.join([stimuli_dir,cond,"left.png"])
     img_right = os.sep.join([stimuli_dir,cond,"right.png"])
     img_neutral =  os.sep.join([stimuli_dir,cond,"neutral.png"])
@@ -57,49 +71,52 @@ for ind, cond in enumerate(condition_list):
     cue_dict = {'left':left_gaze, 'right':right_gaze, 'neutral':neutral_gaze}
 
     # 2) load design parameters
-    filename = os.sep.join([main_dir, 'design', 'task-' + cond + '_counterbalance_ver-01_block-02.csv'])
+    filename = os.sep.join([main_dir, 'design', 'task-pracetice.csv'])
     cb_parameters = pd.read_csv(filename)
     # ______________________________________________________________________________
 
-
-
-    # get
-    start = visual.TextStim(disp, text='Please wait. The game will start shortly.')
+    # start instruction keys
+    start = visual.TextStim(disp, text='Please wait. \nThe game will start shortly.')
+    disp.mouseVisible = False
     start.draw()  # Automatically draw every frame
     disp.flip()
-    event.waitKeys(keyList = 's')
-    event.waitKeys(keyList='5')
+    event.waitKeys(keyList = 's') # experimenter start key - safe key before fMRI trigger
+    event.waitKeys(keyList='5') # fMRI trigger
 
     # 3. set clock _________________________________________________________________
     trialClock = core.Clock()
     kbClock = core.Clock()
     experimentClock = core.Clock()
     experimentClock.reset()
-    # wait 6 scans
+
+    # wait 6 TR, dummy scans
     TR = 0.46
     core.wait(TR*6)
-
+    
     # create dataframe
-    df = pd.DataFrame(columns = ['condition', 'block_order', 'block_number', 'cb_ver', 'fixation_onset', 'cue_direction', 'cue_onset','target_pos', 'target_onset', 'raw_key_response', 'key_rt', 'keypress', 'key_conversion'])
+    df = pd.DataFrame(columns = ['condition', 'block_order', 'block_number', 'cb_ver', 
+    'fixation_onset', 'cue_direction', 'cue_onset','target_pos', 'target_onset', 
+    'raw_key_response', 'key_rt', 'keypress', 'key_conversion'])
     df['condition'] = cb_parameters['cue_type']
     df['block_order'] = ind
-    df['block_number'] = 2
+    df['block_number'] = 1
     df['trial_type'] = cb_parameters['condition_type']
     df['cb_ver'] = cb_parameters['cB_version']
+    
     # loop through design parameters _______________________________________________
     for index, row in cb_parameters.iterrows():
 
         # 4. fixation ______________________________________________________________
         fixation_cross.draw()
         disp.flip()
-        # fixation_onset = experimentClock.getTime()
-        df.loc[index, 'fixation_onset'] = experimentClock.getTime()
+        df.loc[index, 'fixation_onset'] = experimentClock.getTime() # save fixation onset time
         trialClock.reset()
         # 4-1. prepare cue in the background
         df.loc[index, 'cue_direction'] = row.cue_direction
         cue_dict[row.cue_direction].draw()
         while trialClock.getTime() < row.fixation_dur: # fixation duration
             continue
+            
         # 5. cue ___________________________________________________________________
         disp.flip()# cue
         # cue_onset = experimentClock.getTime()
@@ -112,17 +129,16 @@ for ind, cond in enumerate(condition_list):
         target.draw()
         while trialClock.getTime() < 0.2: # cue presentation
             continue
+            
         # 6. target ________________________________________________________________
         disp.flip() # target presented
         target_onset = experimentClock.getTime()
         df.loc[index, 'target_onset'] = target_onset
         trialClock.reset()
-
         kbClock.reset()
+        
         # 7. get response from participant __________________________________________
-
         while trialClock.getTime() <= 2.5: #and not responded:
-
             keys = event.getKeys(keyList = ['1', '2'])
             if len(keys) > 0:
                 print(keys)
@@ -132,23 +148,21 @@ for ind, cond in enumerate(condition_list):
                 df.loc[index, 'keypress'] = keypresstime
                 df.loc[index, 'key_rt'] = rt
                 df.loc[index, 'key_conversion'] = key_map[keys[0]]
-#            ptbKey = kb.getKeys(keyList = ['f', 'j'], waitRelease = False, clear = True)
-#            if ptbKey:
-#                print(ptbKey[0][0], ptbKey[0][1])
-#                df.loc[index, 'raw_key_response'] = ptbKey[0][0]
-#                df.loc[index, 'key_rt'] = ptbKey[0][1]
-#                df.loc[ixndex, 'key_conversion'] = key_map[ptbKey[0][0]]
         kbClock.reset()
-    file_savename = os.sep.join([main_dir, 'data', 'sub-01', 'sub-01_task-' + cond + '_ver-01_block-02_beh.csv'])
+        
+    # 8. save run file __________________________________________
+    # file_savename = os.sep.join([main_dir, 'data', session_info['session_id'], 'practice.csv'])
+    file_savename =  os.sep.join([main_dir, 'data', 'fix_prompt', 'practice.csv'])
     df.to_csv(file_savename)
 
-    # thank you screen
-    message = visual.TextStim(disp, text='Thank you. Please wait for the next game to start')
-    message.draw()  # Automatically draw every frame
+     # 9. End of Run, Wait for experimenter instructions__________________________________________
+    message = visual.TextStim(disp, text='Thank you.\n Please wait for the next game to start')
+    message.draw()  
     disp.flip()
     event.waitKeys(keyList = 'e')
 
-
-message = visual.TextStim(disp, text='This is the end of session2. Please wait for instructions from the experimenter')
+# End of experiment __________________________________________
+message = visual.TextStim(disp, text='This is the end of session 1. \nPlease wait for instructions from the experimenter')
 message.draw()  # Automatically draw every frame
 disp.flip()
+disp.close()
